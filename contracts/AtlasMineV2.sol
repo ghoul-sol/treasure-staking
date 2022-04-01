cef478c48bc2a5546e7e49f52d252d3cf9dc8e00
ghoul-sol <ghoul.sol@pm.me>
2022-03-31 20:32:25 -0500

    QSP-2 fix

--- contracts/AtlasMineV2.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.11;
-
-import './AtlasMine.sol';
-
-contract AtlasMineV2 is AtlasMine {
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() initializer {}
-}
